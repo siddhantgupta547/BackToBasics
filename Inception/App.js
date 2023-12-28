ba7727a7c89f15e6fbf7ed852494c48ef9ab700f
siddhantgupta547 <sidgpt84@gmail.com>
2023-12-28 23:10:35 +0530

    React Basics

---
+++ Inception/App.js
@@ -0,0 +1,15 @@
+const h1 = React.createElement('h1', { id: 'heading' }, 'Namaste React'); // returns an object
+
+const nestedStructure = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', { id: 'h1' }, 'H1 tag'),
+    React.createElement('h2', { id: 'h2' }, 'H2 tag'),
+  ]),
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', { id: 'h1' }, 'H1 tag'),
+    React.createElement('h2', { id: 'h2' }, 'H2 tag'),
+  ]),
+]);
+
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(nestedStructure);
