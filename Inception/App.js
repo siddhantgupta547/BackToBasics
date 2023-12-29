0b1ff0576f53d19ccd0cb7a179f76493a0340e9f
siddhantgupta547 <sidgpt84@gmail.com>
2023-12-29 18:59:04 +0530

    Add jsx and functional components

--- Inception/App.js
+++ Inception/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const h1 = React.createElement('h1', { id: 'heading' }, 'Namaste React'); // returns an object
+// const h1 = React.createElement('h1', { id: 'heading' }, 'Namaste React'); // returns an object
 
-const nestedStructure = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', { id: 'h1' }, 'H1 tag'),
-    React.createElement('h2', { id: 'h2' }, 'H2 tag'),
-  ]),
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', { id: 'h1' }, 'H1 tag'),
-    React.createElement('h2', { id: 'h2' }, 'H2 tag'),
-  ]),
-]);
+// const nestedStructure = React.createElement('div', { id: 'parent' }, [
+//   React.createElement('div', { id: 'child1' }, [
+//     React.createElement('h1', { id: 'h1' }, 'H1 tag'),
+//     React.createElement('h2', { id: 'h2' }, 'H2 tag'),
+//   ]),
+//   React.createElement('div', { id: 'child1' }, [
+//     React.createElement('h1', { id: 'h1' }, 'H1 tag'),
+//     React.createElement('h2', { id: 'h2' }, 'H2 tag'),
+//   ]),
+// ]);
+
+/*------------------------------------------------using JSX-------------------------------------------*/
+
+// const heading = <h1 className="heading">Inception</h1>;
+// console.debug(heading);
+
+/*-----------------------------------------------Adding Functional Components-------------------------------------------*/
+
+function Title() {
+  return <h1 id="heading">Inception</h1>;
+}
+
+function HeadingComponent() {
+  return (
+    <div className="container">
+      <Title />
+      <h2>Functional Component</h2>
+    </div>
+  );
+}
 
 const reactRoot = ReactDOM.createRoot(root);
-reactRoot.render(nestedStructure);
+reactRoot.render(<HeadingComponent />);
